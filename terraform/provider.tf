71a8e22bd40f75bc923f2b1d344e83ad865f6499
Kledson Basso <kledsonhugo@gmail.com>
2025-09-02 16:19:58 -0300

    Atualiza a versão do Terraform para 1.13.1 e do provedor AWS para 6.11.0; ajusta o branch de push para 'test' e remove etapas comentadas do pipeline.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,12 @@
 # PROVIDER
 terraform {
 
-  required_version = "1.12.2"
+  required_version = "1.13.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.9.0"
+      version = "~> 6.11.0"
     }
   }
 
