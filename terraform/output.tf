c11b2a95dfe153452db16ba6af40d195413b77ac
Kledson Basso <kledsonhugo@gmail.com>
2025-09-03 12:31:01 -0300

    Comenta saídas de bucket_name e bucket_id no arquivo output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,10 @@ output "bucket_website_endpoint" {
   value = module.infra.aws_s3_bucket_website_endpoint
 }
 
-output "bucket_name" {
-  value = module.infra.bucket_name
-}
+# output "bucket_name" {
+#   value = module.infra.bucket_name
+# }
 
-output "bucket_id" {
-  value = module.infra.bucket_id
-}
+# output "bucket_id" {
+#   value = module.infra.bucket_id
+# }
